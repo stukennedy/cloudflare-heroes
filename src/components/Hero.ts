7845dee7bde04c1bde11b323d560f8b43e53dc38
Stu Kennedy <s.kennedy@waracle.com>
2023-03-24 10:47:34 +0000

    fix Hero link

--- src/components/Hero.ts
+++ src/components/Hero.ts
@@ -1,26 +1,40 @@
 import { Hero } from '@model/heroes';
 
 export default (hero: Hero) => /*html*/ `
-  <a href="/heroes/${hero.id}" class="w-56">
-    <li class="w-full bg-neutral-200 hover:bg-neutral-300 hover:left-0.5 flex rounded-md relative">
-      <div class="
-        h-full 
-        py-2 px-3 
-        bg-secondary 
-      text-neutral-100 text-sm 
-        rounded-l-md">
-        ${hero.id}
+  <li class="w-56 bg-neutral-200 hover:bg-neutral-300 hover:left-0.5 flex rounded-md relative cursor-pointer">
+    <div class="
+      h-full 
+      py-2 px-3 
+      bg-secondary 
+    text-neutral-100 text-sm 
+      rounded-l-md"
+      hx-get="/heroes/${hero.id}" 
+      hx-push-url="true"
+      hx-target="body"
+      >
+      ${hero.id}
+    </div>
+    <div class="flex justify-between w-full">
+      <div 
+        hx-get="/heroes/${hero.id}" 
+        hx-push-url="true"
+        hx-target="body"
+        class="pt-2 pl-3 text-neutral-800">
+        ${hero.name}
       </div>
-      <div class="flex justify-between w-full ">
-        <div class="pt-2 pl-3 text-neutral-800">${hero.name}</div>
-        <div 
-          class="justify-right bg-white m-1.5 px-2 rounded-md text-neutral-800" 
-          title="delete hero" 
-          hx-delete="/heroes/${hero.id}" 
-          hx-target="#heroes-list">
-          x
-        </div>
+      <div 
+        class="
+          justify-right 
+          text-md 
+        bg-white text-neutral-800 hover:text-white hover:bg-neutral-600
+          m-1.5 mr-2
+          px-2.5 h-6
+          rounded" 
+        title="delete hero" 
+        hx-delete="/heroes/${hero.id}" 
+        hx-target="#heroes-list">
+        x
       </div>
-    </li>
-  </a>
+    </div>
+  </li>
   `;
